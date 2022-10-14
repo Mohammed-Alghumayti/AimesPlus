86edafea6a4f3dc9a3b336b5fe99b974b453f76a
RakanaAlh <93842465+RakanaAlh@users.noreply.github.com>
2022-10-14 17:43:24 +0300

    Create faculty member view model

---
+++ ViewModels/FacultyMembers/FacultyMembersCreateViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace SeniorProject.ViewModels
+{
+    public class FacultyMembersCreateViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(6)]
+        public int AcademicID { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+        
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        
+    }
+}

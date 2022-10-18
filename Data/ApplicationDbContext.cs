c10bcd2c04a7f7d3dc2e3e60cbfbb85d602a6919
RakanaAlh <93842465+RakanaAlh@users.noreply.github.com>
2022-10-18 21:48:22 +0300

    Updated tables

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -9,17 +9,25 @@ namespace SeniorProject.Data
            : base(options) { }
 
         public DbSet<Course> Courses { get; set; }
+        public DbSet<ArticulationMatrix> ArticulationMatrix { get; set; }
         public DbSet<FacultyMembers> FacultyMembers { get; set; }
-        public DbSet<Students> Students { get; set; }
-        public DbSet<Instructors> Instructors { get; set; }
-        public DbSet<Sections> sections { get; set; }
-        public DbSet<CLO> CLO { get; set; }
-        public DbSet<SO> SO { get; set; }
+        public DbSet<ArticulationMatrixActivities> ArticulationMatrixActivities { get; set; }
+        public DbSet<Activities> Activities { get; set; }
+        public DbSet<AssessmentTools> AssessmentTools { get; set; }
+        public DbSet<CourseCatalog> CourseCatalog { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<GradeDistribution> GradeDistribution { get; set; }
         public DbSet<LOD> LOD { get; set; }
-        public DbSet<Topics> Topics { get; set; }
-        public DbSet<InClassAct> inClassActs { get; set; }
-        public DbSet<OutClassAct> OutClassActs { get; set; }
-        public DbSet<AssesmentTools> AssesmentTools { get; set; }
+        public DbSet<Session> Session { get; set; }
+        public DbSet<Student> Student { get; set; }
+        public DbSet<StudentCourse> StudentCourse { get; set; }
+        public DbSet<StudentCourseActivities> StudentCourseActivities { get; set; }
+        public DbSet<StudentCourseAssessmentTools> StudentCourseAssessmentTools { get; set; }
+        public DbSet<Teachers> Teachers { get; set; }
+        public DbSet<TeachersCourse> TeachersCourse { get; set; }
+        public DbSet<TeachingSchedule> TeachingSchedule { get; set; }
+
+       
 
         
     }

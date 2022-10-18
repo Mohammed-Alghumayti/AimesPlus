ec04e04f74146602204f5d9672b2c2c6ea47f57e
MohammedPass <91225132+MohammedPass@users.noreply.github.com>
2022-10-18 21:49:53 +0300

    Add files via upload

--- Controllers/ArticulationMatrixController.cs
+++ Controllers/ArticulationMatrixController.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SeniorProject.Data;
+using SeniorProject.ViewModels;
+using SeniorProject.ViewModels.ArticulationMatrix;
 
 namespace SeniorProject.Controllers
 {
     public class ArticulationMatrixController : Controller
     {
+
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public ArticulationMatrixController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ArticulationMatrixList()
+        {
+            var Arti = applicationDbContext.ArticulationMatrix.ToList();
+
+            var viewModel = new ArticulationMatrixListViewModel
+            {
+                ArticulationMatrix = Arti.Select(a => new ArticulationMatrixListViewModel.ArticulationMatrixItim
+                {
+                    Id = a.Id,
+                    teacherCourse_Ref = a.teacherCourse_Ref,
+                    CLO = a.CLO,
+                    LOD_Ref = a.LOD_Ref,
+                    SO = a.SO,
+                    Assessing_SO = a.Assessing_SO
+                }).ToList()
+            };
+
+            return View(viewModel);
         }
     }
 }

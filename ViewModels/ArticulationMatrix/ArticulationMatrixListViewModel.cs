d1ef17709aa9974fadcf8a63a14a84143bbff56f
MohammedPass <91225132+MohammedPass@users.noreply.github.com>
2022-10-18 22:06:10 +0300

    Add files via upload

---
+++ ViewModels/ArticulationMatrix/ArticulationMatrixListViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SeniorProject.Models;
+using static SeniorProject.ViewModels.CourseListViewModel;
+using System.Collections.Generic;
+
+
+
+namespace SeniorProject.ViewModels.ArticulationMatrix
+{
+    public class ArticulationMatrixListViewModel
+    {
+        public string Query { get; set; }
+        public List<ArticulationMatrixItim> ArticulationMatrix { get; set; }
+
+        public class ArticulationMatrixItim
+        {
+            public int Id { get; set; }
+            public TeachersCourse teacherCourse_Ref { get; set; }
+            public string CLO { get; set; }
+            public LOD LOD_Ref { get; set; }
+            public int SO { get; set; }
+            public bool Assessing_SO { get; set; }
+        }
+       
+    }
+}

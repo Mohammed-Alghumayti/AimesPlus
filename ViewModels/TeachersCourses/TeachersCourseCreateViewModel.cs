054de01292a126360ba7e24cec2064dcc46b7fda
RakanaAlh <93842465+RakanaAlh@users.noreply.github.com>
2022-10-21 19:08:26 +0300

    Teacher course viewmodels
    for each function

---
+++ ViewModels/TeachersCourses/TeachersCourseCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SeniorProject.Models;
+using System.Collections.Generic;
+
+
+namespace SeniorProject.ViewModels
+{
+    public class TeachersCourseCreateViewModel
+    {
+        
+  
+            public int teacherCourse_Id { get; set; }
+            public Course course_Ref { get; set; }
+            public List<SelectListItem> CoursesList { get; set; }
+            public Teachers teacher_Ref { get; set; }
+            public List<SelectListItem> TeachersList { get; set; }
+            public String SemesterStart { get; set; }
+            public String SemesterEnd { get; set; }
+        
+    }
+}
---
+++ ViewModels/TeachersCourses/TeachersCourseListViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SeniorProject.Models;
+using System.Collections.Generic;
+
+
+namespace SeniorProject.ViewModels
+{
+    public class TeachersCourseListViewModel
+    {
+        public List<TeachersCoursesItem> teachersCourses { get; set; }
+
+        public class TeachersCoursesItem
+        {
+            
+            public int teacherCourse_Id { get; set; }
+            public Course course_Ref { get; set; }
+            public Teachers teacher_Ref { get; set; }
+            public String SemesterStart { get; set; }
+            public String SemesterEnd { get; set; }
+        }
+    }
+}

2c8b8be8f627adbb42b5f969aadbe6c7469c804e
MohammedPass <91225132+MohammedPass@users.noreply.github.com>
2022-10-22 02:17:31 +0300

    Add delete function to TeacherCourse

---
+++ ViewModels/TeachersCourses/TeachersCourseDeleteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SeniorProject.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeniorProject.ViewModels
+{
+    public class TeachersCourseDeleteViewModel
+    {
+        
+        public int teacherCourse_Id { get; set; }
+
+        public Course course_Ref { get; set; }   
+        
+        public Teachers teacher_Ref { get; set; }
+
+        public String SemesterStart { get; set; }
+
+        public String SemesterEnd { get; set; }
+
+    }
+}
